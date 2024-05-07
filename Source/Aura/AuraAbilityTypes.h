e0ec8c8b34490203b79655b4ff1768f6d6f2c20e
Lucio <lu30ya31@hotmail.com>
2024-05-07 03:00:02 -0700

    149. Custom Gameplay Effect Context

---
+++ Source/Aura/AuraAbilityTypes.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+
+	FORCEINLINE bool IsCriticalHit() const {return bIsCriticalHit;}
+	FORCEINLINE bool IsBlockedHit() const {return bIsBlockedHit;}
+
+	FORCEINLINE void SetIsCriticalHit(bool bInIsCriticalHit) {bIsCriticalHit = bInIsCriticalHit;}
+	FORCEINLINE void SetIsBlockedHit(bool bInIsBlockedHit) {bIsBlockedHit = bInIsBlockedHit;}
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
