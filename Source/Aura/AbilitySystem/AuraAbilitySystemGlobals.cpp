949e7ee610d46e3e3d48ba5621eb0058891aaaf6
Lucio <lu30ya31@hotmail.com>
2024-05-13 02:40:25 -0700

    153. Aura Ability System Globals

---
+++ Source/Aura/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+// Lucio Martinez
+
+
+#include "AuraAbilitySystemGlobals.h"
+
+#include "Aura/AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
